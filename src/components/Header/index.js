5fd209b840d510f6712087c462917b56fd6e7025
henriqueganz <henrique.ganz@gmail.com>
2023-05-13 21:43:59 -0300

    arquitetura

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+'use client'
+
+import Nav from './styles';
+
+function header() {
+    return(
+        <Nav>
+            <ul>
+                <li>
+                    ala
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+            </ul>
+        </Nav>
+    )
+};
+
+export default header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Nav = styled.nav `
+    margin: 0 auto;
+`;
+
+export default Nav;
+
